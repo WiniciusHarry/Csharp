7d09e4b3f44923a89c1c47ecd860265a861569fa
WiniciusHarry <winiciusharry8@gmail.com>
2024-11-01 16:31:38 -0400

    Tipos de dados nao primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -23,3 +23,44 @@ char inicial = "W";
 
 bool estalogado = true;
 bool ativo = false;
+
+
+//Tipos de dados nao primitivos 
+
+string saudacao = "Ola Mundo!";
+
+//Arrays  
+int[] numeros = {1, 2, 3, 4, 5}
+string[] nomes = {"Luara", "Bhianca", "Jacqueline"}
+
+//Classe 
+public class Pessoa
+{
+    public string nome;
+    public int idade;
+}
+
+//Structs
+public class Ponto
+{
+    public int X;
+    public int Y;
+}
+
+//Enums,
+public enum diaDaSemana
+{
+    Segunda, Terca, Quarta, Quinta, Sexta, Sabado, Domingo
+}
+
+//Interfaces
+public interface IVeiculo
+{
+    void Mover();
+}
+
+//Delegates
+public delegate void Processar(int numero);
+
+//Nullable Types
+int? idade =  null;
