5f9ec21f4755a9696fc3880866d3225d6d30869c
WiniciusHarry <winiciusharry8@gmail.com>
2024-11-01 17:15:36 -0400

    Operadores aritimedicos

--- operadores/Program.cs
+++ operadores/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Operadores aritimedicos
+
+double resultado = 10 + 8 + 12 / 3;
+Console.WriteLine(resultado);
+
+double resultado = (10 + 8 + 12) / 3;
+Console.WriteLine(resultado);
+
+double resultado = (2 + 8) * 2 / 4;
+Console.WriteLine(resultado);
+
