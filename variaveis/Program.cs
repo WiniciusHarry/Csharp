fd30662fbd8b959bf80905e6c2cd897366aaa516
WiniciusHarry <winiciusharry8@gmail.com>
2024-11-01 16:05:15 -0400

    Tipos de dados primitivos

--- variaveis/Program.cs
+++ variaveis/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Tipos de dados primitivos 
+
+short ano = 2025;
+
+int contador = 10;
+
+long populacaoMundial = 7800000000;
+
+//numeros de ponto flutuante
+
+float temperatura = 46.6f;
+
+double precoproduto = 199.90f;
+
+//tipo caracter
+
+char inicial = "W";
+
+//tipo logicos
+
+bool estalogado = true;
+bool ativo = false;
