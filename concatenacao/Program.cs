95eb17885528790c96b367518e1edb6fb48182db
Mauricio Abreu <mauriciogamer163@gmail.com>
2024-11-04 16:59:36 -0400

    Adicionado exemplo de Interpolacao de string

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -2,3 +2,8 @@
 string nome = "Harry"
 string saudacao = "Ola," + nome + "!Bem Vindo."; 
 Console.WriteLine(saudacao);
+
+// Interpolacao de string
+string nome = "Harry"
+string saudacao = $"Ola, {nome} !Bem Vindo."; 
+Console.WriteLine(saudacao);
