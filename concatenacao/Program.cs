8971e5aedcd8eb4377a6615e0ae95932ef9f9c5a
Mauricio Abreu <mauriciogamer163@gmail.com>
2024-11-04 17:21:20 -0400

    Adicoes (getType, Typeof, Parse)

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -7,3 +7,17 @@ Console.WriteLine(saudacao);
 string nome = "Harry"
 string saudacao = $"Ola, {nome} !Bem Vindo."; 
 Console.WriteLine(saudacao);
+
+// Operador GetType()
+int numero = 5;
+Console.WriteLine(numero.GetType());
+
+// Palavra-chave typeof
+Console.WriteLine(typeof(int));
+
+// Metodo Parse
+string valorTexto = "100";
+Console.WriteLine("A variavel valorTexto e do tipo:" + valorTexto.GetType());
+
+int numero = int.Parse(valorTexto.);
+Console.WriteLine("A variavel numeroParse e do tipo:" + valorTexto.GetType());
