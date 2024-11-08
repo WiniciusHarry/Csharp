ffcdb62c8bffc5ae063b4e734c2d3f96d3b988aa
WiniciusHarry <winiciusharry8@gmail.com>
2024-11-08 14:42:08 -0400

    Switch case add

--- estruturasCondicionais/Program.cs
+++ estruturasCondicionais/Program.cs
@@ -1,33 +1,34 @@
-﻿// double nota1 = 8.0;
-// double nota2 = 8.0;
-// double nota3 = 6.8;
-// double media = (nota1 + nota2 + nota3) / 3;
-// if (media >= 7)
-// {
-//     Console.WriteLine($"Media {media:F2}: Aprovado");
-// }
-// else
-// {
-//     Console.WriteLine($"Media {media:F2}: Recuperacao");
-// }
+﻿// Estrutura Condicional IF e ELSE
+double nota1 = 8.0;
+double nota2 = 8.0;
+double nota3 = 6.8;
+double media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7)
+{
+     Console.WriteLine($"Media {media:F2}: Aprovado");
+ }
+else
+{
+     Console.WriteLine($"Media {media:F2}: Recuperacao");
+}
 
-// // Exemplo Alinhado
-// double nota1 = 8.0;
-// double nota2 = 8.0;
-// double nota3 = 5.0;
-// double media = (nota1 + nota2 + nota3) / 3;
-// if (media >= 7)
-// {
-//     Console.WriteLine($"Media {media:F2}: Aprovado");
-// }
-// else if (media >= 5 && media < 7)   
-// {
-//     Console.WriteLine($"Media {media:F2}: Recuperacao");
-// }
-// else
-// {
-//     Console.WriteLine($"Media {media:F2}: Reprovado");
-// }
+// Exemplo Alinhado
+double nota1 = 8.0;
+double nota2 = 8.0;
+double nota3 = 5.0;
+double media = (nota1 + nota2 + nota3) / 3;
+if (media >= 7)
+{
+    Console.WriteLine($"Media {media:F2}: Aprovado");
+}
+else if (media >= 5 && media < 7)   
+{
+     Console.WriteLine($"Media {media:F2}: Recuperacao");
+}
+ else
+{
+     Console.WriteLine($"Media {media:F2}: Reprovado");
+}
 
 //Exemplo if, else e else if e operador logico
 Console.WriteLine("Digite uma vogal");
@@ -55,4 +56,41 @@ else if(letra == "u" || letra == "U")
 else
 {
     Console.WriteLine($"A letra {letra} nao e uma vogal");
+}
+
+// Estrutura Condicional Switch Case
+
+string dia = "Sabado";
+
+switch (dia)
+{
+    case "Sabado":
+        Console.WriteLine("Sabadoooo galera!");
+        break;
+    case "Domingo":
+        Console.WriteLine("Domingao, dia de dormir o dia todo");
+        break;
+    default:
+        Console.WriteLine("Hoje e dia de semana, levanta da cama e para de preguica")
+        break;
+}
+
+//  Exemplo 2:
+
+Console.WriteLine("Digite uma vogal:");
+string? letra = Console.ReadLine();
+string letraMinuscula = letra.ToLower();
+//string letraMaiuscula = letra.ToUpper();
+switch (letraMinuscula)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine($"A letra '{letraMinuscula}' e uma vogal.");
+        break;
+    default:
+        Console.WriteLine($"A letra '{letraMinuscula}' nao e uma vogal.");
+        break;
 }
