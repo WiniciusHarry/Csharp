0fa6701919761298d8bc98358eaa39841c04a442
RuiSilva2 <eduardojoshua023@gmail.com>
2024-11-11 15:06:27 -0400

    add Estrutura de Repitcao For

--- estruturasDeRepeticao/Program.cs
+++ estruturasDeRepeticao/Program.cs
@@ -1,10 +1,10 @@
 ﻿// EStrutura de Repitcao While
 Random numeroAleaotrio = new Random();
 int numero = numeroAleaotrio.Next(1, 101);
-int contador = 90;
+int contador = 0;
 Console.WriteLine($"Numero Alvo: {numero}");
 // Loop que incrementa o contador ate ele atingir o numero alvo
-while (contador > numero)
+while (contador < numero)
 {
     Console.WriteLine($"Contador: {contador}.");
     contador++;
@@ -18,3 +18,10 @@ do
 {
     Consoleo.WriteLine($"Numero: {numero}");
 } while (numero < 10);
+
+// Estrutura de Repitcao For
+// for (INICIALIZACAO; CONDICAO; IMPLEMENTO)
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"Valor de i: {i}");
+}
