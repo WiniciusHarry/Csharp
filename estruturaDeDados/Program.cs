10fd05c05d6e088ef965a956ebecdcf3174f2906
WiniciusHarry <winiciusharry8@gmail.com>
2024-11-13 16:25:13 -0400

    Estrutura de Dados add

---
+++ estruturaDeDados/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] meuArray = new int[5];
+meuArray[0] = 10;
+meuArray[1] = 20;
+meuArray[2] = 30;
+meuArray[3] = 40;
+meuArray[4] = 50;
+Console.WriteLine("Numero no array: ");
+for (int i = 0; i < meuArray.Length; i++)
+{
+    Console.WriteLine(meuArray[i]);
+}
